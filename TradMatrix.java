f9de330924ff1f6c3c661b1c5af1a20bd9bf1a2a
Acniea <bq2367@gmail.com>
2017-07-26 13:11:00 -0700

    Update TradMatrix.java

--- TradMatrix.java
+++ TradMatrix.java
@@ -4,9 +4,7 @@ package hsap.csr;
 public class TradMatrix{
     private double[][] mat;
     
-    public TradMatrix(double[][] m){
-        mat = m;
-    }
+    public TradMatrix(double[][] m){mat = m;}
     
     public TradMatrix(CSRMatrix m){
         mat = new double[m.numRows()][m.numCols()];
@@ -17,15 +15,10 @@ public class TradMatrix{
         }
     }
     
-    public double at(int row, int col){
-        return mat[row][col];
-    }
+    public double at(int row, int col){return mat[row][col];}
     
     public static TradMatrix randomSparseMatrix(int numEntries, int numCols, int numRows, double[] weights){
-        if(numEntries > numRows*numCols){
-            numEntries = numRows*numCols;
-        }
-        
+        if(numEntries > numRows*numCols){numEntries = numRows*numCols;}
         double[][] data = new double[numRows][numCols];
         int column = (int)(Math.random()*numCols);
         int offset = 1;
@@ -39,35 +32,40 @@ public class TradMatrix{
             offset++;
         }
         for(int i=0; i<numEntries-offset; i++){
-            int row = (int)(Math.random()*numRows);
-            int col = (int)(Math.random()*numCols);
+            int row = (int)(Math.random()*numRows), col = (int)(Math.random()*numCols);
             while(data[row][col]!=0){
-                row = (int)(Math.random()*numRows);
-                col = (int)(Math.random()*numCols);
+                row = (int)(Math.random()*numRows); col = (int)(Math.random()*numCols);
             }
             data[row][col] = weights[(int)(Math.random()*weights.length)];
         }
         return new TradMatrix(data);
     }
-
-    public int numRows() {
-        return mat.length;
+    
+    public static TradMatrix randomSparseGraph(int numEntries, int n, double[] weights){
+        if(numEntries > n*n){numEntries = n*n;}
+        double[][] data = new double[n][n];
+        for(int i=0; i<numEntries; i++){
+            int row = (int)(Math.random()*n), col = (int)(Math.random()*n);
+            while(data[row][col]!=0 || row>col || row==col){
+                row = (int)(Math.random()*n); col = (int)(Math.random()*n);
+            }
+            int index = (int)(Math.random()*weights.length);
+            data[row][col] = weights[index]; data[col][row] = weights[index];
+        }
+        return new TradMatrix(data);
     }
 
-    public int numCols() {
-        return mat[0].length;
-    }
+    public int numRows(){return mat.length;}
+
+    public int numCols(){return mat[0].length;}
     
     public String toString(){
-        String s = "";
+        String s = "[";
         for(int i=0; i<numRows(); i++){
-            s+="[";
-            for(int j=0; j<numCols(); j++){
-                s+=(int)mat[i][j]+" ";
-            }
+            for(int j=0; j<numCols(); j++){s+=(int)mat[i][j]+"\t";}
             s=s.substring(0,s.length()-1);
-            s+="]\n";
+            s+="]\n[";
         }
-        return s.substring(0,s.length()-1);
+        return s.substring(0,s.length()-2);
     }
 }
